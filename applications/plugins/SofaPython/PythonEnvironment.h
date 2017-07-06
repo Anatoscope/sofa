f47e02fbeb8c6f83e61c52330a31be93ae31fef9
Maxime Tournier <maxime.tournier@anatoscope.com>
2017-07-06 18:09:08 +0200

    [SofaPython] added gil raii

--- applications/plugins/SofaPython/PythonEnvironment.h
+++ applications/plugins/SofaPython/PythonEnvironment.h
@@ -86,6 +86,19 @@ public:
     };
 
     struct system_exit : std::exception { };
+
+    /// use this RAII-class to ensure the gil is properly acquired and released
+    /// in a scope. these should be surrounding any python code called from c++,
+    /// i.e. in all the methods in PythonEnvironment and all the methods in
+    /// PythonScriptController.
+    class gil {
+        const PyGILState_STATE state;
+    public:
+        gil();
+        ~gil();
+    };
+    
+
 };
 
 
