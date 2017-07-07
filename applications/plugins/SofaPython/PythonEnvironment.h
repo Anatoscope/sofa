435f9151dde9576b226691457d782065ea7780ce
Maxime Tournier <maxime.tournier@anatoscope.com>
2017-07-07 17:45:14 +0200

    [SofaPython] oh damn you windows

--- applications/plugins/SofaPython/PythonEnvironment.h
+++ applications/plugins/SofaPython/PythonEnvironment.h
@@ -91,7 +91,7 @@ public:
     /// in a scope. these should be surrounding any python code called from c++,
     /// i.e. in all the methods in PythonEnvironment and all the methods in
     /// PythonScriptController.
-    class gil {
+    class SOFA_SOFAPYTHON_API gil {
         const PyGILState_STATE state;
         const char* const trace;
     public:
@@ -100,7 +100,7 @@ public:
     };
 
 
-    class no_gil {
+    class SOFA_SOFAPYTHON_API no_gil {
         PyThreadState* const state;
         const char* const trace;
     public:
