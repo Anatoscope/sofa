afcd5604d1173ac165b0769cec94d11c8a62b37d
Maxime Tournier <maxime.tournier@anatoscope.com>
2017-09-04 18:01:33 +0200

    [SofaPython] console cleanup

--- applications/plugins/SofaPython/python/SofaPython/console.py
+++ applications/plugins/SofaPython/python/SofaPython/console.py
@@ -1,11 +1,13 @@
 """a simple threaded readline console module (unix only)"""
 
-def start(local = None, history = None):
+def start(local = None, history = '~/.sofa_console'):
     
     import threading
     import atexit
     import os
     import code
+    import sys
+    import signal
     
     def target():
         
@@ -17,15 +19,19 @@ def start(local = None, history = None):
         if history:
             import readline
             filename = os.path.expanduser( history )
-            readline.read_history_file( filename )
-
+            try:
+                readline.read_history_file( filename )
+            except IOError as e:
+                pass
+                
             def write_history():
                 readline.write_history_file( filename )
 
             atexit.register( write_history )
             
         code.interact(local = local)
-
+        os.kill(os.getpid(), signal.SIGINT)
+        
     thread = threading.Thread(target = target)
     thread.daemon = True
     thread.start()
