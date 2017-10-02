33971a17d8eeb08984c047d5c8ac1622b8e6f19d
Maxime Tournier <maxime.tournier@anatoscope.com>
2017-10-02 11:10:01 +0200

    [Copmliant] DifferenceFromTargetMapping instantiations on Vec6

--- applications/plugins/Compliant/mapping/DifferenceFromTargetMapping.cpp
+++ applications/plugins/Compliant/mapping/DifferenceFromTargetMapping.cpp
@@ -21,11 +21,13 @@ using namespace defaulttype;
 int DifferenceFromTargetMappingClass = core::RegisterObject("Compute position differences between dofs and targets")
         .addAlias("OffsetMapping") // backward compatibility with a previous identical mapping
 #ifndef SOFA_FLOAT
+.add< DifferenceFromTargetMapping< Vec6dTypes, Vec6dTypes > >()    
 .add< DifferenceFromTargetMapping< Vec3dTypes, Vec3dTypes > >()
 .add< DifferenceFromTargetMapping< Vec1dTypes, Vec1dTypes > >()
 .add< DifferenceFromTargetMapping< Rigid3dTypes, Vec3dTypes > >()
 #endif
 #ifndef SOFA_DOUBLE
+.add< DifferenceFromTargetMapping< Vec6fTypes, Vec6fTypes > >()    
 .add< DifferenceFromTargetMapping< Vec3fTypes, Vec3fTypes > >()
 .add< DifferenceFromTargetMapping< Vec1fTypes, Vec1fTypes > >()
 .add< DifferenceFromTargetMapping< Rigid3fTypes, Vec3fTypes > >()
@@ -34,12 +36,14 @@ int DifferenceFromTargetMappingClass = core::RegisterObject("Compute position di
 
 
 #ifndef SOFA_FLOAT
+template class SOFA_Compliant_API DifferenceFromTargetMapping< Vec6dTypes, Vec6dTypes >;
 template class SOFA_Compliant_API DifferenceFromTargetMapping< Vec3dTypes, Vec3dTypes >;
 template class SOFA_Compliant_API DifferenceFromTargetMapping< Vec1dTypes, Vec1dTypes >;
 template class SOFA_Compliant_API DifferenceFromTargetMapping< Rigid3dTypes, Vec3dTypes >;
 #endif
 
 #ifndef SOFA_DOUBLE
+template class SOFA_Compliant_API DifferenceFromTargetMapping< Vec6fTypes, Vec6fTypes >;
 template class SOFA_Compliant_API DifferenceFromTargetMapping< Vec3fTypes, Vec3fTypes >;
 template class SOFA_Compliant_API DifferenceFromTargetMapping< Vec1fTypes, Vec1fTypes >;
 template class SOFA_Compliant_API DifferenceFromTargetMapping< Rigid3fTypes, Vec3fTypes >;
